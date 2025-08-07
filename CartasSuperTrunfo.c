e45369291f35548d90491957d33a4a2bda26f4e3
Samuel Cardoso <samuelmouracardosoh@gmail.com>
2025-08-07 13:18:46 -0300

    escolha da carta para comparar

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,7 +3,7 @@
 //Perguntar, armazenar variaveis das cartas e calcular renda per capta e densidade populacional 
 int main(){
 //Carta 001
-char Estado001[2];
+char Estado001[5];
 char Codigo001[3];
 char Nome001[20];
 unsigned long int População001;
@@ -15,7 +15,7 @@ float PiBper001;
 float poder001;
 
 //Carta 002
-char Estado002[2];
+char Estado002[5];
 char Codigo002[3];
 char Nome002[20];
 unsigned long int População002;
@@ -25,6 +25,9 @@ int PT002;
 float densidade002;
 float PiBper002;
 float poder002;
+
+//Comparação de uma unica carta
+int comparacao;
 
 
 //Cadastro da Carta 001
@@ -129,7 +132,7 @@ printf("Carta 002\n:");
 printf("Estado: %s\n", Estado002);
 printf("Código: %s\n", Codigo002);
 printf("Nome da Cidade: %s\n", Nome002);
-printf("População: %d\n", População002);
+printf("População: %ld\n", População002);
 printf("Área: %.2f km²\n", área002);
 printf("PiB: %.2f  bilhões de reais\n", PiB002);
 printf("Número de pontos turísticos: %d\n", PT002);
@@ -137,8 +140,95 @@ printf("Densidade Populacional: %.2f hab/km²\n", densidade002);
 printf("PiB per Capita: %.2f reais\n", PiBper002);
 printf("O Poder da sua carta e: %.2f\n", poder002);
 
-//comparar variaveis das cartas
+//Comparação de uma unica carta:
+printf("Escolha um atributo para comparar:\n 1: População\n 2: Área\n 3: PiB\n 4: Pontos Turisticos\n 5: Densidade\n 6: Pib per capita\n 7: Poder\n ");
+scanf("%d", &comparacao);
+
 //População
+if (comparacao == 1)
+{printf("Atributo: População\n Carta 1 - %s (%s): %ld\n Carta 2 - %s (%s): %ld\n", Nome001, Estado001, População001, Nome002, Estado002, População002);
+    if (População001 > População002)
+    {
+        printf("Carta 1 e a vencedora\n");
+    } else
+    {
+        printf("Carta 2 e a vencedora\n");
+    }
+}
+
+//Area
+if (comparacao == 2)
+{printf("Atributo: Área\n Carta 1 - %s (%s): %.2f\n Carta 2 - %s (%s): %.2f\n", Nome001, Estado001, área001, Nome002, Estado002, área002);
+    if (área001 > área002)
+    {
+        printf("Carta 1 e a vencedora\n");
+    } else
+    {
+        printf("Carta 2 e a vencedora\n");
+    }
+}
+
+//PiB
+if (comparacao == 3)
+{printf("Atributo: PiB\n Carta 1 - %s (%s): %.2f\n Carta 2 - %s (%s): %.2f\n", Nome001, Estado001, PiB001, Nome002, Estado002, PiB002);
+    if (PiB001 > PiB002)
+    {
+        printf("Carta 1 e a vencedora\n");
+    } else
+    {
+        printf("Carta 2 e a vencedora\n");
+    }
+}
+
+//Pontos Turisticos
+if (comparacao == 4)
+{printf("Atributo: Pontos Turisticos\n Carta 1 - %s (%s): %d\n Carta 2 - %s (%s): %d\n", Nome001, Estado001, PT001, Nome002, Estado002, PT002);
+    if (PT001 > PT002)
+    {
+        printf("Carta 1 e a vencedora\n");
+    } else
+    {
+        printf("Carta 2 e a vencedora\n");
+    }
+}
+
+//Densidade
+if (comparacao == 5)
+{printf("Atributo: Densidade\n Carta 1 - %s (%s): %.2f\n Carta 2 - %s (%s): %.2f\n", Nome001, Estado001, densidade001, Nome002, Estado002, densidade002);
+    if (densidade001 < densidade002)
+    {
+        printf("Carta 1 e a vencedora\n");
+    } else
+    {
+        printf("Carta 2 e a vencedora\n");
+    }
+}
+
+//Renda per Capita
+if (comparacao == 6)
+{printf("Atributo: Renda per Capita\n Carta 1 - %s (%s): %.2f\n Carta 2 - %s (%s): %.2f\n", Nome001, Estado001, PiBper001, Nome002, Estado002, PiBper002);
+    if (PiBper001 > PiBper002)
+    {
+        printf("Carta 1 e a vencedora\n");
+    } else
+    {
+        printf("Carta 2 e a vencedora\n");
+    }
+}
+
+//Poder
+if (comparacao == 7)
+{printf("Atributo: Poder\n Carta 1 - %s (%s): %.2f\n Carta 2 - %s (%s): %.2f\n", Nome001, Estado001, poder001, Nome002, Estado002, poder002);
+    if (poder001 > poder002)
+    {
+        printf("Carta 1 e a vencedora\n");
+    } else
+    {
+        printf("Carta 2 e a vencedora\n");
+    }
+}
+
+/*
 printf("População:");
 if (População001 > População002){
  printf("Carta 1 venceu\n");
@@ -206,9 +296,7 @@ if (poder001 > poder002){
 else
 {
  printf("Carta 2 venceu\n");
-}
+}*/
 
 return 0;
 }
-
-//Na pagina do trabalho não dizia nada sobre usar if e o else então resolvi aprender sobre pra facilitar, mas se houver problema eu refaço da outra maneira
